69f071e914d707428fe7684f051bff5153d86464
Nguyen Duc Loc <nguyenducloc404@gmail.com>
2023-05-26 13:50:00 +0700

    Remove unnecessary import

--- client/src/components/Layout/Content.jsx
+++ client/src/components/Layout/Content.jsx
@@ -1,8 +1,7 @@
 import { Layout } from "antd";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Breadcrumb from "../Content/Breadcrumb";
 import classes from "./../../styles/Layout/Content.module.css";
-import { useState } from "react";
 
 const { Content } = Layout;
 
