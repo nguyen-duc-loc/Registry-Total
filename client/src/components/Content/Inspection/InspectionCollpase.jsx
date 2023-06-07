81bdfa0eae418eafdbe90da98e8810609dfb7f1a
Nguyen Duc Loc <nguyenducloc404@gmail.com>
2023-06-07 16:38:27 +0700

    Change view inspection collapse based on new API

--- client/src/components/Content/Inspection/InspectionCollpase.jsx
+++ client/src/components/Content/Inspection/InspectionCollpase.jsx
@@ -35,6 +35,96 @@ const InspectionCollapse = (props) => {
 
   const breakPoint = useMediaQuery({ query: "(max-width: 768px)" });
 
+  const items = [
+    {
+      key: "owner",
+      label: "Thông tin chủ sở hữu",
+      children: <Owner owner={owner} />,
+    },
+    {
+      key: "car",
+      label: "Phương tiện",
+      children: <Car car={car} showLink={true} carId={car.id} />,
+    },
+    {
+      key: "specification",
+      label: "Thông số kĩ thuật",
+      children: <Specification specification={specification} />,
+    },
+    {
+      key: "inspection",
+      label: "Thông tin đăng kiểm",
+      children: (
+        <Descriptions
+          column={1}
+          style={{ padding: "1.2rem" }}
+          layout={breakPoint ? "vertical" : "horizontal"}
+        >
+          <Item
+            label={
+              <TextWithIcon
+                Icon={IoShieldCheckmarkOutline}
+                text="Số đăng kiểm"
+              />
+            }
+          >
+            {data.inspectionNumber}
+          </Item>
+          <Item
+            label={
+              <TextWithIcon Icon={IoCalendarOutline} text="Ngày đăng kiểm" />
+            }
+          >
+            {processDate(data.inspectionDate)}
+          </Item>
+          <Item
+            label={
+              <TextWithIcon
+                Icon={IoCalendarOutline}
+                text="Có hiệu lực đến ngày"
+              />
+            }
+          >
+            {processDate(data.expiredDate)}
+          </Item>
+          <Item
+            label={
+              <TextWithIcon Icon={IoConstructOutline} text="Đơn vị kiểm định" />
+            }
+          >
+            {data.madeBy.workFor.name}
+          </Item>
+          <Item
+            label={<TextWithIcon Icon={IoCallOutline} text="Số điện thoại" />}
+          >
+            {data.madeBy.workFor.phone}
+          </Item>
+          <Item label={<TextWithIcon Icon={IoMailOutline} text="Email" />}>
+            {data.madeBy.workFor.email}
+          </Item>
+          <Item
+            label={
+              <TextWithIcon Icon={IoPersonOutline} text="Nhân viên đăng kiểm" />
+            }
+          >
+            {data.madeBy.name}
+          </Item>
+          {data.firstTime && (
+            <Item
+              label={
+                <TextWithIcon Icon={IoSyncOutline} text="Đăng kiểm lần đầu" />
+              }
+            >
+              <div>
+                <IoCheckmark style={{ color: "#379237" }} />
+              </div>
+            </Item>
+          )}
+        </Descriptions>
+      ),
+    },
+  ];
+
   return (
     <div
       style={{
@@ -44,94 +134,11 @@ const InspectionCollapse = (props) => {
       }}
     >
       <Collapse
+        items={items}
         defaultActiveKey="inspection"
         accordion
         style={{ width: "100%" }}
-      >
-        <Panel header="Thông tin chủ sở hữu" key="owner">
-          <Owner owner={owner} />
-        </Panel>
-        <Panel header="Phương tiện" key="car">
-          <Car car={car} showLink={true} carId={car.id} />
-        </Panel>
-        <Panel header="Thông số kĩ thuật" key="specification">
-          <Specification specification={specification} />
-        </Panel>
-        <Panel header="Thông tin đăng kiểm" key="inspection">
-          <Descriptions
-            column={1}
-            style={{ padding: "1.2rem" }}
-            layout={breakPoint ? "vertical" : "horizontal"}
-          >
-            <Item
-              label={
-                <TextWithIcon
-                  Icon={IoShieldCheckmarkOutline}
-                  text="Số đăng kiểm"
-                />
-              }
-            >
-              {data.inspectionNumber}
-            </Item>
-            <Item
-              label={
-                <TextWithIcon Icon={IoCalendarOutline} text="Ngày đăng kiểm" />
-              }
-            >
-              {processDate(data.inspectionDate)}
-            </Item>
-            <Item
-              label={
-                <TextWithIcon
-                  Icon={IoCalendarOutline}
-                  text="Có hiệu lực đến ngày"
-                />
-              }
-            >
-              {processDate(data.expiredDate)}
-            </Item>
-            <Item
-              label={
-                <TextWithIcon
-                  Icon={IoConstructOutline}
-                  text="Đơn vị kiểm định"
-                />
-              }
-            >
-              {data.madeBy.workFor.name}
-            </Item>
-            <Item
-              label={<TextWithIcon Icon={IoCallOutline} text="Số điện thoại" />}
-            >
-              {data.madeBy.workFor.phone}
-            </Item>
-            <Item label={<TextWithIcon Icon={IoMailOutline} text="Email" />}>
-              {data.madeBy.workFor.email}
-            </Item>
-            <Item
-              label={
-                <TextWithIcon
-                  Icon={IoPersonOutline}
-                  text="Nhân viên đăng kiểm"
-                />
-              }
-            >
-              {data.madeBy.name}
-            </Item>
-            {data.firstTime && (
-              <Item
-                label={
-                  <TextWithIcon Icon={IoSyncOutline} text="Đăng kiểm lần đầu" />
-                }
-              >
-                <div>
-                  <IoCheckmark style={{ color: "#379237" }} />
-                </div>
-              </Item>
-            )}
-          </Descriptions>
-        </Panel>
-      </Collapse>
+      />
       <InspectionPDF car={car} specification={specification} data={data} />
     </div>
   );
