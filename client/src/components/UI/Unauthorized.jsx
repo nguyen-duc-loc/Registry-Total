38e846f966e2b17b1c5f161f88cf09161f3a65f5
Nguyen Duc Loc <nguyenducloc404@gmail.com>
2023-06-13 14:45:15 +0700

    Update error message

--- client/src/components/UI/Unauthorized.jsx
+++ client/src/components/UI/Unauthorized.jsx
@@ -1,26 +1,26 @@
-import {Button, Result} from "antd";
-import {useNavigate} from "react-router-dom";
-import {useEffect} from 'react';
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Unauthorized = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        document.title = "403 - Bạn không có quyền truy cập trang web này"
-    }, [])
+  useEffect(() => {
+    document.title = "403 - Bạn không có quyền truy cập trang web này";
+  }, []);
 
-    return (
-        <Result
-            status="403"
-            title="403"
-            subTitle="Xin lỗi, bạn không có quyền truy cập trang web này."
-            extra={
-                <Button type="primary" onClick={() => navigate("/")}>
-                    Trở về trang chủ
-                </Button>
-            }
-        />
-    );
+  return (
+    <Result
+      status="403"
+      title="403"
+      subTitle="Xin lỗi, bạn không có quyền truy cập trang web này."
+      extra={
+        <Button type="primary" onClick={() => navigate("/")}>
+          Trở về trang chủ
+        </Button>
+      }
+    />
+  );
 };
 
 export default Unauthorized;
