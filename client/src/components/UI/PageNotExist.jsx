9dc802276efd9a9ab9856b845aadb35fb4bcc457
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-06-13 09:28:27 +0700

    Change title when visit error page

--- client/src/components/UI/PageNotExist.jsx
+++ client/src/components/UI/PageNotExist.jsx
@@ -1,8 +1,13 @@
 import { Button, Result } from "antd";
 import { useNavigate } from "react-router-dom";
+import {useEffect} from 'react';
 
 const PageNotExist = () => {
   const navigate = useNavigate();
+
+  useEffect(()=> {
+      document.title = "404 - Trang web này không tồn tại"
+  },[])
 
   return (
     <Result
--- client/src/components/UI/Unauthorized.jsx
+++ client/src/components/UI/Unauthorized.jsx
@@ -1,21 +1,26 @@
-import { Button, Result } from "antd";
-import { useNavigate } from "react-router-dom";
+import {Button, Result} from "antd";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from 'react';
 
 const Unauthorized = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <Result
-      status="403"
-      title="403"
-      subTitle="Xin lỗi, bạn không có quyền truy cập trang web này."
-      extra={
-        <Button type="primary" onClick={() => navigate("/")}>
-          Trở về trang chủ
-        </Button>
-      }
-    />
-  );
+    useEffect(() => {
+        document.title = "403 - Bạn không có quyền truy cập trang web này"
+    }, [])
+
+    return (
+        <Result
+            status="403"
+            title="403"
+            subTitle="Xin lỗi, bạn không có quyền truy cập trang web này."
+            extra={
+                <Button type="primary" onClick={() => navigate("/")}>
+                    Trở về trang chủ
+                </Button>
+            }
+        />
+    );
 };
 
 export default Unauthorized;
