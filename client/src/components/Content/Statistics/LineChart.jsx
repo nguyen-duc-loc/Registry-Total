d0923fb05346ede1e5ee8c59237a06e3749b650d
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-06-13 09:16:34 +0700

    Fix get a centre

--- client/src/components/Content/Statistics/LineChart.jsx
+++ client/src/components/Content/Statistics/LineChart.jsx
@@ -1,87 +1,83 @@
-import { Line } from "@ant-design/plots";
-import { Card } from "antd";
-import { useState, useEffect } from "react";
-import { useAuthHeader, useAuthUser } from "react-auth-kit";
+import {Line} from "@ant-design/plots";
+import {Card} from "antd";
+import {useState, useEffect} from "react";
+import {useAuthHeader, useAuthUser} from "react-auth-kit";
 
 const LineChart = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const authHeader = useAuthHeader();
-  const auth = useAuthUser();
-  const admin = auth().data.role === "admin";
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const authHeader = useAuthHeader();
+    const auth = useAuthUser();
+    const admin = auth().data.role === "admin";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
 
-        const response = await fetch(
-          `${import.meta.env.VITE_BASE_URL}/api/v1/inspections/${
-            admin
-              ? "allCentresStatistics/year?sort=year"
-              : "centreStatistics/monthYear?sort=year,month"
-          }`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: authHeader(),
-            },
-          }
-        );
+                const response = await fetch(
+                    `${import.meta.env.VITE_BASE_URL}/api/v1/inspections/${
+                        admin
+                            ? "allCentresStatistics"
+                            : "centreStatistics"
+                    }/monthYear?sort=year,month&limit=100000`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: authHeader(),
+                        },
+                    }
+                );
 
-        if (!response.ok) {
-          throw new Error("Can not get.");
-        }
+                if (!response.ok) {
+                    throw new Error("Can not get.");
+                }
 
-        const res = await response.json();
+                const res = await response.json();
 
-        const dataItems = res.data.data.map((d) => {
-          if (admin) {
-            return d;
-          } else {
-            return {
-              count: d.count,
-              monthYear: `${d.month}/${d.year}`,
-            };
-          }
-        });
+                const dataItems = res.data.data.map((d) => {
+                    return {
+                        count: d.count,
+                        monthYear: `${d.month}/${d.year}`,
+                    };
+                });
 
-        setData(dataItems);
+                setData(dataItems);
 
-        setLoading(false);
-      } catch (err) {
-        setLoading(false);
-        if (import.meta.env.VITE_ENV === "development")
-          console.error("Can not get.");
-      }
-    };
+                setLoading(false);
+            } catch (err) {
+                setLoading(false);
+                if (import.meta.env.VITE_ENV === "development")
+                    console.error("Can not get.");
+            }
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <Card title="Biểu đồ đăng kiểm" loading={loading}>
-      <Line
-        data={data}
-        xField={admin ? "year" : "monthYear"}
-        yField="count"
-        height={300}
-        xAxis={{
-          tickCount: 5,
-        }}
-        slider={{
-          start: admin ? 0 : 0.5,
-          end: 1,
-        }}
-        meta={{
-          count: {
-            alias: "Số lượng",
-          },
-        }}
-        smooth={true}
-      />
-    </Card>
-  );
+    return (
+        <Card title="Biểu đồ đăng kiểm" loading={loading}>
+            <Line
+                data={data}
+                xField="monthYear"
+                yField="count"
+                height={300}
+                xAxis={{
+                    tickCount: 5,
+                }}
+                slider={{
+                    start: 0.5,
+                    end: 1,
+                }}
+                meta={{
+                    count: {
+                        alias: "Số lượng",
+                    },
+                }}
+                smooth={true}
+            />
+        </Card>
+    );
 };
 
 export default LineChart;
